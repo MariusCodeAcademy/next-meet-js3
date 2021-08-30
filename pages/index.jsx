8e2924aa72900063e7bdd4c8a534b7eaa7b73ad2
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-30 10:44:42 +0300

    rendering problem

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 const DUMMY_MEETUPS = [
@@ -18,12 +19,21 @@ const DUMMY_MEETUPS = [
 ];
 
 const HomePage = () => {
+  const [meetupsArr, setMeetupsArr] = useState([]);
+  useEffect(() => {
+    setMeetupsArr(DUMMY_MEETUPS);
+  }, []);
   return (
     <>
       <h1>Home meetup page</h1>
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={meetupsArr} />
     </>
   );
 };
+const pass = 'asdasd';
+// tam kad puslapis butu sugeneruotas duomenims pasikeitus yra naudojami 2 budai
+
+// SSR server side rendering - duomenys sugeneruojami uzklausom metus, tinka labiau kai duomenys kinta kas sekunde ar greiciau
+// SSG static site Generatingas = duomeny sugeneruojami aplikacijos sukurimo metu ir atnaujinami jei reikia tam tikru intervalu
 
 export default HomePage;
