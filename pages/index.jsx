2fb414f72875483a645cc260b6b32a96fc404b78
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-08-30 09:32:28 +0300

    meetupList

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,27 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'The first meetup',
+    image: 'https://picsum.photos/id/1016/1000/800',
+    address: 'Some street 5, 2328282, Rome, Italy',
+    description: 'This is our first meet in Italy',
+  },
+  {
+    id: 'm2',
+    title: 'The Engalnd meetup',
+    image: 'https://picsum.photos/id/1018/1000/800',
+    address: 'Highbury road 5, 2328282, London, England',
+    description: 'This is our first meet in UK',
+  },
+];
+
 const HomePage = () => {
   return (
     <>
       <h1>Home meetup page</h1>
+      <MeetupList meetups={DUMMY_MEETUPS} />
     </>
   );
 };
